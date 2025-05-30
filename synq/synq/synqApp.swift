0c37fe83f860b3375a76350c840ff1fa4625d4ed
lukesmith344 <lukesmith34@uchicago.edu>
2025-05-30 09:38:03 -0400

    initial

---
+++ synq/synq/synqApp.swift
@@ -0,0 +1,17 @@
+//
+//  synqApp.swift
+//  synq
+//
+//  Created by Luke Smith on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct synqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ synq/synqTests/synqTests.swift
@@ -0,0 +1,16 @@
+//
+//  synqTests.swift
+//  synqTests
+//
+//  Created by Luke Smith on 5/30/25.
+//
+
+import Testing
+
+struct synqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
